fcb022aed51269a1589b80618b53387fef412f4b
Austin Luper <austin.luper@gmail.com>
2023-12-21 21:29:02 -0500

    Added YouTube Data API v3 API key and its respective URL to the Javascript file. Added relevant comments to these changes. Additional changes will likely need to be made to the called API URL to coordinate with our other Javascript logic and HTML elements.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// This variable will be used to hold the YouTube Data API v3 API key
+const youtubeKey = "AIzaSyD6d-Ut49ZFUr3fQg_9BytQ3Eg4ttNQu3E";
+
+// This constructs the API URL which will need to be called whenever user
+// searches for a movie title ** PRETTY SURE THAT WE WILL NEED TO HAVE 
+// QUERY PARAMETERS ADJUSTED SO THAT IT DYNAMICALLY ACCEPTS USER INPUT**
+
+const youtubeApiUrl = `https://www.googleapis.com/youtube/v3`
