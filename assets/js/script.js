eed5031b08d8f504460b62461cb49b7e5160f4e7
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-01-04 20:24:44 -0500

    Updating files back to original

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Declare global variables
+var searchHistory = document.getElementById("history-list");
+var movies = JSON.parse(localStorage.getItem("movies")) || [];
+
 // Function to get movie title from API call
 function getMovieTitle(movieTitle) {
 	// The following code snippet is from the RapidAPI, MOVIEDATABASE API. The following
@@ -23,17 +27,44 @@ function getMovieTitle(movieTitle) {
     });
 };
 
+// Function snippet and function to get movie trailer from Movie API
+function getMovieTrailer(movieTitle) {
+	const settings = {
+		async: true,
+		crossDomain: true,
+		url: 'https://moviesdatabase.p.rapidapi.com/titles/search/title/' + movieTitle + '?exact=true&titleType=movie&info=rating',
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'ca79fa9bf9msh9d23e3356f2b48ep1f9043jsn1218866eabb6',
+			'X-RapidAPI-Host': 'movie87.p.rapidapi.com'
+		}
+	};
+	// Initiates AJAX request based on the above "settings" object
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+};
+
 // Function to save search history
-
-
+function saveHistory(movieTitle) {
+	movieTitle = movieTitle.toLowerCase();
+	if (movies.includes(movieTitle) === false) {
+		movies.push(movieTitle);
+		var movieJSON = JSON.stringify(movies);
+		localStorage.setItem("movie", movieJSON);
+		displayHistory();
+	};
+};
 
 // Function to display search history
-
-
+function displayHistory() {
+	searchHistory.textContent = "";
+};
 
 // Event listener for submit button
 $(".button").on("click", function() {
 	var movieTitle = $(".input").val(); 
 	getMovieTitle(movieTitle);
+	// Clears search input value
 	$(".input").val("");
 });
